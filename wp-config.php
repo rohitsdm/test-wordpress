78619a882fc0634b1a17e22a21158fed4d9c58d1
rohitsdm <rohitk.kumar@smartdatainc.net>
2021-09-27 19:55:14 +0530

    config change

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'Tshdvfi5262(FSxfajq' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sdeitest.online' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
