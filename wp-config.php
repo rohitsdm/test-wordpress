94619ff1be80064f79155ba746b6539375c2acc4
rohitsdm <rohitk.kumar@smartdatainc.net>
2021-09-27 20:17:09 +0530

    config change db change

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'Tshdvfi5262(FSxfajq' );
 
 /** MySQL hostname */
-define( 'DB_HOST', '44.230.11.93' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
